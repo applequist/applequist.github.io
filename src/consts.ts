24e80dca7c5192a7369cd43fe1f62246daad403d
Brieuc Desoutter <brieuc.desoutter@gmail.com>
2024-05-13 22:23:25 +0300

    Complete rewrite with Astro.

---
+++ src/consts.ts
@@ -0,0 +1,7 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'brieuc.desoutter';
+export const SITE_DESCRIPTION = 'Hi! Welcome to my little corner of the Internet!';
+export const GH_URL = 'https://github.com/Applequist';
+export const LINKEDIN_URL = 'https://www.linkedin.com/in/bdesoutter';
--- themes/basic/archetypes/posts.md
+++
@@ -1,9 +0,0 @@
-+++
-title = '{{ replace .File.ContentBaseName "-" " " | title }}'
-description = 'Short description of the content of the post'
-author = 'Brieuc Desoutter'
-date = {{ .Date }}
-lastmod = {{ .Date }}
-draft = true
-+++
-
