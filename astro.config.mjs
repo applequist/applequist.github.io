0a3521d77b93ca1291ba8079d6485bf8f00f6bae
Brieuc Desoutter <brieuc.desoutter@gmail.com>
2024-10-04 12:47:37 +0300

    Add Google analytics... properly.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,21 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
+import partytown from '@astrojs/partytown';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://Applequist.github.io',
 	trailingSlash: 'always',
-	integrations: [mdx(), sitemap()],
+	integrations: [
+		mdx(), 
+		sitemap(), 
+		partytown({
+			config: {
+				forward: ["dataLayer.push"],
+			}
+		})
+	],
 	markdown: {
 		syntaxHighlight: 'prism'
 	}
